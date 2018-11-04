8b375da6f8cd169f44fbef1a437b323ad7ebbae4
James Peake <peakejw@vcu.edu>
2018-11-04 11:19:24 -0500

    actor class

--- IMDBPredict/src/Actor.java
+++ IMDBPredict/src/Actor.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Actor {
+    private String name;
+    private ArrayList<String> title;
+    private double score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getTitle() {
+        return title;
+    }
+
+    public void setTitle(ArrayList<String> title) {
+        this.title = title;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
 }
